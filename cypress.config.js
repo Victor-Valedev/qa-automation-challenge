2c31ecbf87cbb6fa62bf1156b65ef50b94574410
Victor-Valedev <victorvaledev@gmail.com>
2025-10-27 14:38:32 -0300

    config: atualiza config do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: true, // grava vídeos automaticamente durante o run
+  videosFolder: "cypress/videos", // pasta onde os vídeos serão salvos
+
+  viewportWidth: 1366, // largura da tela
+  viewportHeight: 768,  // altura da tela
+
   e2e: {
-    // URL base da aplicação
-    baseUrl: "https://seubarriga.wcaquino.me",
-    
+    baseUrl: "https://seubarriga.wcaquino.me", // URL base da aplicação
+
     setupNodeEvents(on, config) {
+      // eventos e hooks futuros, se necessário
     },
   },
 });
+
 
